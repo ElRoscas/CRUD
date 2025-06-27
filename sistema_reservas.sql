eca69a41f1172c00da636fe135d3ad0f5a7d4e37
ElRocas <110638779+ElRoscas@users.noreply.github.com>
2025-06-27 09:17:15 +0200

    Update sistema_reservas.sql

--- sistema_reservas.sql
+++ sistema_reservas.sql
@@ -17,15 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `sistema_reservas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empleados`
---
 
 CREATE TABLE `empleados` (
   `id` int(11) NOT NULL,
@@ -34,20 +25,12 @@ CREATE TABLE `empleados` (
   `departamento` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Volcado de datos para la tabla `empleados`
---
 
 INSERT INTO `empleados` (`id`, `nombre`, `email`, `departamento`) VALUES
 (1, 'Laura Pérez', 'laura.perez@empresa.com', 'Marketing'),
 (2, 'Carlos López', 'carlos.lopez@empresa.com', 'IT'),
 (3, 'Ana Torres', 'ana.torres@empresa.com', 'RRHH');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `reservas`
---
 
 CREATE TABLE `reservas` (
   `id` int(11) NOT NULL,
@@ -58,18 +41,10 @@ CREATE TABLE `reservas` (
   `hora_fin` time NOT NULL
 ) ;
 
---
--- Volcado de datos para la tabla `reservas`
---
 
 INSERT INTO `reservas` (`id`, `id_empleado`, `id_sala`, `fecha`, `hora_inicio`, `hora_fin`) VALUES
 (1, 1, 2, '2025-06-25', '10:00:00', '11:30:00');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `salas`
---
 
 CREATE TABLE `salas` (
   `id` int(11) NOT NULL,
@@ -78,69 +53,34 @@ CREATE TABLE `salas` (
   `recursos` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Volcado de datos para la tabla `salas`
---
+
 
 INSERT INTO `salas` (`id`, `nombre`, `capacidad`, `recursos`) VALUES
 (1, 'Sala Ágora', 10, 'Proyector, Pizarra'),
 (2, 'Sala Neptuno', 20, 'TV, Conferencia'),
 (3, 'Sala Marte', 8, 'Pizarra, Sillas ergonómicas');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `empleados`
---
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- Indices de la tabla `reservas`
---
 ALTER TABLE `reservas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_empleado` (`id_empleado`),
   ADD KEY `id_sala` (`id_sala`);
 
---
--- Indices de la tabla `salas`
---
 ALTER TABLE `salas`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `empleados`
---
 ALTER TABLE `empleados`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT de la tabla `reservas`
---
 ALTER TABLE `reservas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `salas`
---
 ALTER TABLE `salas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `reservas`
---
 ALTER TABLE `reservas`
   ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`id_sala`) REFERENCES `salas` (`id`) ON DELETE CASCADE;
